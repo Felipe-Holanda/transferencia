ca4c0ecf85360037eb78d3e22b63d18ee441ad23
Felipe Holanda <contact.felipeholanda@gmail.com>
2025-09-10 16:49:27 -0300

    feat: 🎸 implementa status nas transações, para melhorias
    Notei que há um erro estrutural na lógica dos agendamentos e na forma
    como a aplicação estava trabalhando-a. Ela estaria debitando o saldo do
    usuário mesmo quando a a transação era agendada. Causando uma quebra de
    lógica com o que é esperado com as aplicações comuns desta mesma
    estirpe.

--- backend/src/main/java/com/bank/evolve/repository/TransactionRepository.java
+++ backend/src/main/java/com/bank/evolve/repository/TransactionRepository.java
@@ -1,10 +1,15 @@
 package com.bank.evolve.repository;
 
 import com.bank.evolve.entity.User;
+import com.bank.evolve.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.bank.evolve.entity.Transaction;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findBySenderOrRecipient(User sender, User recipient);
+    List<Transaction> findByTargetDateAndStatus(LocalDate targetDate, TransactionStatus status);
+    List<Transaction> findByTargetDateBeforeAndStatus(LocalDate targetDate, TransactionStatus status);
+    List<Transaction> findBySenderAndStatus(User sender, TransactionStatus status);
 }
